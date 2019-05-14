0091cb709e34706084734cf17b347900ea0a4dd0
dalmano <dalmano@outlook.com>
2019-05-14 18:10:39 +0100

    create database Relates #48

---
+++ src/model/database/db_build.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const dbConnection = require("./db_connection");
+const sql = fs.readFileSync(`${__dirname}/db_build.sql`).toString();
+
+dbConnection.query(sql, (err, res) => {
+  if (err) throw err;
+  console.log("Gymbuddy table built: ", res);
+});
---
+++ src/model/database/db_connections.js
@@ -0,0 +1,16 @@
+const { pool } = require("pg");
+const url = require("url");
+require("env2")("./config.env");
+
+let connectionString =
+  process.env.NODE_ENV === "test"
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL;
+
+if (!connectionString) throw new Error("Database url must be set");
+
+module.exports = new Pool({
+  connectionString,
+  max: 2,
+  ssl: !connectionString.includes("localhost")
+});
