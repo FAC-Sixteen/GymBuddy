68c155cba7a83973655ad246738f8871c0541074
dalmano <dalmano@outlook.com>
2019-05-15 14:23:00 +0100

    feature-build-database

--- src/model/database/db_build.js
+++ src/model/database/db_build.js
@@ -1,8 +1,17 @@
 const fs = require("fs");
-const dbConnection = require("./db_connection");
+const dbConnection = require("./db_connections");
 const sql = fs.readFileSync(`${__dirname}/db_build.sql`).toString();
 
-dbConnection.query(sql, (err, res) => {
-  if (err) throw err;
-  console.log("Gymbuddy table built: ", res);
-});
+const runDbBuild = () =>
+  new Promise((resolve, reject) => {
+    dbConnection.query(sql, (err, res) => {
+      if (err) reject(err);
+      console.log("Gymbuddy table built");
+
+      resolve(true);
+    });
+  });
+
+runDbBuild();
+
+module.exports = runDbBuild;
--- src/model/database/db_connections.js
+++ src/model/database/db_connections.js
@@ -1,12 +1,12 @@
-const { pool } = require("pg");
+const { Pool } = require("pg");
 const url = require("url");
 require("env2")("./config.env");
 
 let connectionString =
   process.env.NODE_ENV === "test"
-    ? process.env.TEST_DATABASE_URL
-    : process.env.DATABASE_URL;
-
+    ? process.env.TEST_DB_URL
+    : process.env.DB_URL;
+console.log(connectionString);
 if (!connectionString) throw new Error("Database url must be set");
 
 module.exports = new Pool({
