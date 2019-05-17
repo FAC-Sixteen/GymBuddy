9d27d00ca1eba9175e3a9e2922371e6a11785e20
dalmano <dalmano@outlook.com>
2019-05-17 11:15:05 +0100

    sort goals object

--- src/model/queries/getMatchUsers.js
+++ src/model/queries/getMatchUsers.js
@@ -48,7 +48,7 @@ const completeUserData = (
   timeID
 ) => {
   let resultArr = [];
-  console.log(resultArr);
+
   let matchedUser = returnMatchedUsers(age, gender, location, experience);
   matchedUser.then(result =>
     setTimeout(() => {
@@ -65,31 +65,32 @@ const completeUserData = (
   userTime.then(result => result);
   resultArr.push(userTime);
 
-  // let promisesArray = [matchedUser, userGoal, userTime];
-
   Promise.all(resultArr).then(value => {
     mapData(value);
-    // console.log(value);
   });
 };
 
 completeUserData(28, "Male", "London", "Beginner", 2, 2);
 
+const sortObj = (obj = "{ users_id: 2, goals_id: '' }") => {
+  const goal = obj.goals_id;
+  return goal;
+};
+
 const mapData = value => {
   let objOne = { ...value[0][0] };
-  let objTwo = { ...value[1] };
-  let objThree = { ...value[1][1] };
-  const { goals_id } = objTwo;
+  let goalsObjOne = { ...value[1][0] };
+  let goalsObjTwo = { ...value[1][1] };
+  let goalsObjThree = { ...value[1][2] };
+  let goalsObjFour = { ...value[1][3] };
 
-  // console.log(value);
-  objOne.goals_id = [goals_id];
+  const firstGoal = sortObj(goalsObjOne);
+  const secondGoal = sortObj(goalsObjTwo);
+  const thirdGoal = sortObj(goalsObjThree);
+  const fourthGoal = sortObj(goalsObjFour);
 
-  // const { goals_id } = objThree;
-  // objOne.goals_id[1] = [goals_id];
-
-  // console.log(objOne);
-  // console.log(objTwo);
-  // console.log(objThree);
+  objOne.goals_id = [firstGoal, secondGoal, thirdGoal, fourthGoal];
+  console.log(objOne);
 };
 
 module.exports = {
