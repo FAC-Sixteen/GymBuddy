d1d57014044f567b0fe0c54edd6da62415ad8faa
dalmano <dalmano@outlook.com>
2019-05-21 11:35:53 +0100

    create congrats page Relates #76

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,29 +1,28 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const error = require('./error');
+const error = require("./error");
 
-
-const splash = require('./splashPage');
-const conductPage = require('./conductPage');
+const splash = require("./splashPage");
+const conductPage = require("./conductPage");
 const signup = require("./signupPage");
-const createProfilePage = require('./createProfilePage');
-const location = require('./location');
+const createProfilePage = require("./createProfilePage");
+const location = require("./location");
+const congratsPage = require("./congratsPage");
 
-
-router.post('/locate', location.post); 
+router.post("/locate", location.post);
 router.get("/", splash.get);
 router.get("/conduct-page", conductPage.get);
 router.get("/sign-up", signup.get);
+router.get("/congrats-page", congratsPage.get);
 
-
-router.post('/:gender-gender/:time-time', (req, res, next) => {
-    // console.log('params', req.params);
-    console.log('body', req.body);
-    // console.log(req)
-    res.redirect('/create-profile-page/');
+router.post("/:gender-gender/:time-time", (req, res, next) => {
+  // console.log('params', req.params);
+  console.log("body", req.body);
+  // console.log(req)
+  res.redirect("/create-profile-page/");
 });
 
-router.get('/create-profile-page', createProfilePage.get);
+router.get("/create-profile-page", createProfilePage.get);
 
 router.use(error.client);
 router.use(error.server);
