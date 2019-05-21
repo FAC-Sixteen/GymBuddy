bb30092583b9b21cdb7e3321e1f21d9d220771ba
dalmano <dalmano@outlook.com>
2019-05-21 15:56:55 +0100

    create report page Relates #89

--- src/controller/index.js
+++ src/controller/index.js
@@ -6,14 +6,15 @@ const splash = require("./splashPage");
 const conductPage = require("./conductPage");
 const signup = require("./signupPage");
 const matchBuddiesPage = require("./matchBuddiesPage");
-const location = require('./location');
-const postUser = require('./postUserImage');
+const location = require("./location");
+const postUser = require("./postUserImage");
 const congratsPage = require("./congratsPage");
 const createProfilePage = require("./createProfilePage");
+const reportPage = require("./reportPage");
 
 //POST
-router.post('/locate', location.post); 
-router.post('/create-profile', postUser.post)
+router.post("/locate", location.post);
+router.post("/create-profile", postUser.post);
 
 //GET
 router.get("/", splash.get);
@@ -22,7 +23,7 @@ router.get("/sign-up", signup.get);
 router.get("/congrats-page", congratsPage.get);
 router.get("/create-profile-page", createProfilePage.get);
 router.get("/match-buddies-page", matchBuddiesPage.get);
-
+router.get("/report-page", reportPage.get);
 
 router.use(error.client);
 router.use(error.server);
