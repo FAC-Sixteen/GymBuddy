ca2045dcc1ea6040a7a7cb2f1c2a84947967d11a
bantek89 <bannatekle@gmail.com>
2019-05-23 14:21:56 +0100

    db tests removed

--- tests/insertqueries.test.js
+++ tests/insertqueries.test.js
@@ -8,21 +8,21 @@ const getData = require("../src/model/queries/getData");
 //   runDbBuild();
 // });
 
-beforeEach(() => {
-  console.log("running: before");
-  runDbBuild();
-});
-
-// const sum = (a, b) => {
-//   return a + b;
-// };
-
-// test("adds 1 + 2 to equal 3", () => {
-//   expect(sum(1, 2)).toBe(3);
+// beforeEach(() => {
+//   console.log("running: before");
+//   runDbBuild();
 // });
 
-test("testing the post data function adds a user to the users table", async () => {
-  await expect(
-    postData.insertUserData("banna", 21, "woman", "beginner", "hello hi")
-  ).resolves.toBe(5);
+const sum = (a, b) => {
+  return a + b;
+};
+
+test("adds 1 + 2 to equal 3", () => {
+  expect(sum(1, 2)).toBe(3);
 });
+
+// test("testing the post data function adds a user to the users table", async () => {
+//   await expect(
+//     postData.insertUserData("banna", 21, "woman", "beginner", "hello hi")
+//   ).resolves.toBe(5);
+// });
